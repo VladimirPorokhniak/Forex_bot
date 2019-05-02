a896a57f50d5744cc6a5917a6633ea88aa95dc5f
vladimir <porokhnyak.v.yu@gmail.com>
2019-05-02 07:31:20 +0300

    "make it better"

--- forex_bot.py
+++ forex_bot.py
@@ -8,16 +8,16 @@ import time
 coll_neyronov_na_uravne = 1
 coll_skrytyh_uravney = 2
 Constanta = 1
-Iteration = 15
+Iteration = 10
 Epoch = 10000000
 speed_of_learn = 1
 Mistake_of_learn_new = 0
 Mistake_of_learn_old = 1000
 Mistake_of_test_new = 0
 Mistake_of_test_old = 1000
-answer_of_predict = 0
-status_of_work = False
-status_of_data = 0
+Mistake_of_NET = 1000
+predicted_answer = -1
+status_of_full = 0
 Data_test = [0 for i in range(Iteration + coll_neyronov_na_uravne)]
 Data_learn = [0 for i in range(Iteration + coll_neyronov_na_uravne)]
 Neyron = [[0 for i11 in range(coll_neyronov_na_uravne)] for i12 in range(coll_skrytyh_uravney + 1)]
@@ -26,8 +26,8 @@ OUT_Neyron = 0
 Weights = []
 for i23 in range(coll_skrytyh_uravney):
     Weights.append(
-        [[randomer(True) for i21 in range(coll_neyronov_na_uravne)] for i22 in range(coll_neyronov_na_uravne)])
-Weight = [randomer(True) for i in range(coll_neyronov_na_uravne)]
+        [[0 for i21 in range(coll_neyronov_na_uravne)] for i22 in range(coll_neyronov_na_uravne)])
+Weight = [0 for i in range(coll_neyronov_na_uravne)]
 Weights_d = []
 for i23 in range(coll_skrytyh_uravney):
     Weights_d.append([[0 for i21 in range(coll_neyronov_na_uravne)] for i22 in range(coll_neyronov_na_uravne)])
@@ -40,13 +40,29 @@ Weight_old = [0 for i in range(coll_neyronov_na_uravne)]
 bot = telebot.TeleBot(TOKEN, threaded=False)
 
 
+def random_weight():
+    global Weights, Weight, coll_skrytyh_uravney, coll_neyronov_na_uravne, Mistake_of_test_old, Mistake_of_learn_old, speed_of_learn, Mistake_of_learn_new, Mistake_of_test_new
+    for i1 in range(coll_skrytyh_uravney):
+        for i2 in range(coll_neyronov_na_uravne):
+            for i3 in range(coll_neyronov_na_uravne):
+                Weights[i1][i2][i3] = randomer()
+    for i1 in range(coll_neyronov_na_uravne):
+        Weight[i1] = randomer()
+    speed_of_learn = 1
+    Mistake_of_learn_old = 1000
+    Mistake_of_test_old = 1000
+    Mistake_of_learn_new = 0
+    Mistake_of_test_new = 0
+
+
 def Data_for_learn() -> object:
     global Data_test, Data_learn, Iteration, coll_neyronov_na_uravne, Constanta
     index = 0
     size = coll_neyronov_na_uravne + Iteration
     while index < size:
         now = datetime.datetime.now()
-        if now.minute in [4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59] and now.second == 58 and (955000 < now.microsecond <= 960000):
+        if now.minute in [4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59] and now.second == 58 and (
+                955000 < now.microsecond <= 960000):
             b = curs_online()
             if not b == (-1):
                 Data_learn[index] = b - Constanta
@@ -56,7 +72,8 @@ def Data_for_learn() -> object:
     index = 0
     while index < size:
         now = datetime.datetime.now()
-        if now.minute in [4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59] and now.second == 58 and (955000 < now.microsecond <= 960000):
+        if now.minute in [4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59] and now.second == 58 and (
+                955000 < now.microsecond <= 960000):
             b = curs_online()
             if not b == (-1):
                 Data_test[index] = b - Constanta
@@ -66,23 +83,23 @@ def Data_for_learn() -> object:
 
 
 def change_curs():
-    global Neyron, status_of_work, coll_neyronov_na_uravne, answer_of_predict, status_of_data
-    status_of_work = True
+    global Neyron, status_of_full, coll_neyronov_na_uravne, predicted_answer
     for i in range(1, coll_neyronov_na_uravne - 1):
         Neyron[0][i] = Neyron[0][i + 1]
     a = curs_online()
     if not (a == (-1)):
         Neyron[0][coll_neyronov_na_uravne - 1] = a - Constanta
-        if status_of_data < (coll_neyronov_na_uravne - 1):
-            status_of_data += 1
+        if status_of_full < (coll_neyronov_na_uravne - 1):
+            status_of_full += 1
     else:
         for i in range(1, coll_neyronov_na_uravne):
             Neyron[0][i] = 0
-        status_of_data = 0
-    status_of_work = False
+        status_of_full = 0
+    if status_of_full == (coll_neyronov_na_uravne - 1):
+        predicted_answer = test_net(True)
 
 
-def predict(index) -> object:
+def test_net(index) -> object:
     OUT_Neyron_Test = 0
     global Neyron, Weight, Weights, coll_neyronov_na_uravne, coll_skrytyh_uravney, Mistake_of_test_new, OUT_Neyron
     for j1 in range(1, coll_skrytyh_uravney + 1):
@@ -101,7 +118,7 @@ def predict(index) -> object:
 
 
 def save(index):
-    global Weight, Weights, Weight_old, Weights_old
+    global Weight, Weights, Weight_old, Weights_old, coll_neyronov_na_uravne, coll_skrytyh_uravney
     if index:
         for j1 in range(0, coll_skrytyh_uravney):
             for j2 in range(0, coll_neyronov_na_uravne):
@@ -118,21 +135,21 @@ def save(index):
             Weight[j3] = Weight_old[j3]
 
 
-def Testy(index, iteraciya):
-    global Data_learn, Data_test, Iteraciya, Neyron, OUT_Neyron
+def testy(index, iteration):
+    global Data_learn, Data_test, Neyron, OUT_Neyron
     if index:
         for i in range(coll_neyronov_na_uravne):
-            Neyron[0][i] = Data_learn[iteraciya + i]
-        OUT_Neyron = Data_learn[iteraciya + coll_neyronov_na_uravne]
+            Neyron[0][i] = Data_learn[iteration + i]
+        OUT_Neyron = Data_learn[iteration + coll_neyronov_na_uravne]
     else:
         for i in range(coll_neyronov_na_uravne):
-            Neyron[0][i] = Data_test[iteraciya + i]
-        OUT_Neyron = Data_test[iteraciya + coll_neyronov_na_uravne]
+            Neyron[0][i] = Data_test[iteration + i]
+        OUT_Neyron = Data_test[iteration + coll_neyronov_na_uravne]
 
 
 def learn_net():
     OUT_Neyron_Learn = 0
-    global Neyron, Weight, Weights, coll_neyronov_na_uravne, coll_skrytyh_uravney, Mistake_of_test_new, Weight_d, Weights_d
+    global Neyron, Weight, Weights, coll_neyronov_na_uravne, coll_skrytyh_uravney, Mistake_of_learn_new, Weight_d, Weights_d
     for j1 in range(1, coll_skrytyh_uravney + 1):
         for j2 in range(0, coll_neyronov_na_uravne):
             Neyron[j1][j2] = 0
@@ -142,7 +159,7 @@ def learn_net():
     for j in range(0, coll_neyronov_na_uravne):
         OUT_Neyron_Learn += (Neyron[coll_skrytyh_uravney][j] * Weight[j])
     OUT_Neyron_Learn = round(activation(OUT_Neyron_Learn), 5)
-    Mistake_of_test_new += (abs(1 - (OUT_Neyron_Learn / OUT_Neyron)) * 100)
+    Mistake_of_learn_new += (abs(1 - (OUT_Neyron_Learn / OUT_Neyron)) * 100)
     OUT_Mistake = proizvodnaya(OUT_Neyron_Learn) * (OUT_Neyron - OUT_Neyron_Learn)
     for i1 in range(coll_neyronov_na_uravne):
         Weight_d[i1] = speed_of_learn * OUT_Mistake * Neyron[coll_skrytyh_uravney][i1]
@@ -167,21 +184,18 @@ def learn_net():
         Weight[j3] += Weight_d[j3]
 
 
-def Learn():
-    global Epoch, Iteration, Mistake_of_test_new, Mistake_of_test_old, Mistake_of_learn_old, Mistake_of_learn_new, speed_of_learn
-    bot.send_message(Chat_id_root, "Сбор данных...")
-    Data_for_learn()
-    bot.send_message(Chat_id_root, "Сбор данных завершен")
-    bot.send_message(Chat_id_root, "Обучение нейронной сети...")
-    for epoha in range(Epoch):
+def Learning():
+    global Epoch, Iteration, Mistake_of_test_new, Mistake_of_test_old, Mistake_of_learn_old, Mistake_of_learn_new, speed_of_learn, Mistake_of_NET
+    random_weight()
+    for epoch in range(Epoch):
         save(True)
         for iteration in range(Iteration):
-            Testy(True, iteration)
+            testy(True, iteration)
             learn_net()
         Mistake_of_learn_new /= Iteration
         for iteration in range(Iteration):
-            Testy(False, iteration)
-            predict(False)
+            testy(False, iteration)
+            test_net(False)
         Mistake_of_test_new /= Iteration
         if Mistake_of_learn_new < Mistake_of_learn_old:
             Mistake_of_learn_old = Mistake_of_learn_new
@@ -194,9 +208,20 @@ def Learn():
             Mistake_of_test_old = Mistake_of_test_new
         elif Mistake_of_test_new > Mistake_of_test_old:
             save(False)
-            bot.send_message(Chat_id_root, str(Mistake_of_test_old))
+            Mistake_of_NET = Mistake_of_test_old
             break
+
+
+def Learn():
+    global Mistake_of_NET
+    bot.send_message(Chat_id_root, "Сбор данных...")
+    Data_for_learn()
+    bot.send_message(Chat_id_root, "Сбор данных завершен")
+    bot.send_message(Chat_id_root, "Обучение нейронной сети...")
+    while Mistake_of_NET > 0.08:
+        Learning()
     bot.send_message(Chat_id_root, "Обучение нейронной сети завершено")
+    bot.send_message(Chat_id_root, "Ошибка сети: " + str(Mistake_of_NET))
 
 
 @bot.message_handler(commands=['start'])
@@ -219,26 +244,20 @@ def stop(message: Message):
 @bot.message_handler(func=lambda message: True)
 def answer_to_message(message: Message):
     answer = message.text
-    global Neyron, status_of_work, status_of_data, minute
+    global predicted_answer, status_of_full
     if answer in ["curs", "Курс", "курс"]:
         bot.reply_to(message, "Курс Евро к Доллару:\n" + str(curs_online()))
     elif answer in ["predict", "Прогнозировать", "прогнозировать"]:
-        if not status_of_work:
-            if status_of_data == (coll_neyronov_na_uravne - 1):
-                bot.send_message(message.chat.id,
-                                 "Прогнозируемый курс Евро к Доллару через 5 минут:\n" + str(predict(True)))
-            else:
-                Time = coll_neyronov_na_uravne - status_of_data - 1
-                if (Time % 10 == 1) and (not Time == 11):
-                    minute = " минута!"
-                elif (Time % 10 in [2, 3, 4]) and (not (Time in [12, 13, 14])):
-                    minute = " минуты!"
-                elif (Time % 10 >= 5) or (Time % 10 == 0) or (Time in [11, 12, 13, 14]):
-                    minute = " минут!"
-                bot.reply_to(message,
-                             "Мы не готовы к прогнозированию! \n Повторите попытку через " + str(Time) + minute)
+        if not predicted_answer == (-1):
+            Time = datetime.datetime.now()
+            Timer = Time.hour * 60 + Time.minute + 5 - Time.minute % 5
+            h = Timer // 60
+            m = Timer % 60
+            bot.send_message(message.chat.id,
+                             "Прогнозируемый курс Евро к Доллару на " + str(h) + ":" + str(m) + " :\n" + str(predicted_answer + Constanta))
         else:
-            bot.reply_to(message, "Повторите попытку через 5 секунд!")
+            bot.reply_to(message,
+                         "Мы не готовы к прогнозированию! \n Повторите попытку через 5 минут")
     else:
         bot.reply_to(message, "Запрос не понятен!")
 
--- testing.py
+++ testing.py
@@ -1,7 +1,15 @@
 import datetime
 import telebot
-from Function import *
+from math import exp
+import os
+import random
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+import threading
 
+TOKEN = '706327123:AAHuPNEyFC57Y5XbIXOWCQ32e6ii9Gfpon0'
+Chat_id_root = 448157691
+DeltaTime = 3
 coll_neyronov_na_uravne = 1
 coll_skrytyh_uravney = 2
 Constanta = 1
@@ -37,15 +45,40 @@ Weight_old = [0 for i in range(coll_neyronov_na_uravne)]
 bot = telebot.TeleBot(TOKEN, threaded=False)
 
 
-def start() -> object:
-    global Data_learn, Data_test, Iteration, coll_neyronov_na_uravne
-    b = Iteration + coll_neyronov_na_uravne
-    with open("learn.txt") as learn:
-        for i in range(b):
-            Data_learn[i] = float(learn.readline()) - Constanta
-    with open("test.txt") as test:
-        for i in range(b):
-            Data_test[i] = float(test.readline()) - Constanta
+def curs_online(curs='eur-usd'):
+    wd = webdriver.PhantomJS(executable_path=os.path.abspath('phantomjs'))
+    url = "https://www.fxclub.org/markets/forex/" + curs + "/"
+    wd.get(url)
+    WebDriverWait(wd, 1)
+    page = wd.page_source
+    wd.quit()
+    index = page.find("Котировка:")
+    a = page[index + 11:index + 200]
+    index = a.find("</span>")
+    a = a[index - 10:index]
+    a = a[a.find(">") + 1:index]
+    a = a.replace(",", '.')
+    try:
+        a = round(float(a), 5)
+    except ValueError:
+        a = -1
+    return a
+
+
+def activation(x):
+    a = (1 / (1 + exp(-x)))
+    return a
+
+
+def proizvodnaya(x):
+    return x * (1 - x)
+
+
+def randomer() -> object:
+    b = 10000
+    a = random.randint(0 - b, b)
+    a /= (b / 10)
+    return a
 
 
 def random_weight():
@@ -69,7 +102,8 @@ def Data_for_learn() -> object:
     size = coll_neyronov_na_uravne + Iteration
     while index < size:
         now = datetime.datetime.now()
-        if now.minute in [4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59] and now.second == 58 and (955000 < now.microsecond <= 960000):
+        if now.minute in [4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59] and now.second == 58 and (
+                955000 < now.microsecond <= 960000):
             b = curs_online()
             if not b == (-1):
                 Data_learn[index] = b - Constanta
@@ -79,7 +113,8 @@ def Data_for_learn() -> object:
     index = 0
     while index < size:
         now = datetime.datetime.now()
-        if now.minute in [4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59] and now.second == 58 and (955000 < now.microsecond <= 960000):
+        if now.minute in [4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59] and now.second == 58 and (
+                955000 < now.microsecond <= 960000):
             b = curs_online()
             if not b == (-1):
                 Data_test[index] = b - Constanta
@@ -220,14 +255,14 @@ def Learning():
         elif Mistake_of_test_new > Mistake_of_test_old:
             save(False)
             Mistake_of_NET = Mistake_of_test_old
+            bot.send_message(Chat_id_root, "Ошибка сети-пробника: " + str(Mistake_of_NET))
             break
 
 
 def Learn():
     global Mistake_of_NET
     bot.send_message(Chat_id_root, "Сбор данных...")
-    # Data_for_learn()
-    start()
+    Data_for_learn()
     bot.send_message(Chat_id_root, "Сбор данных завершен")
     bot.send_message(Chat_id_root, "Обучение нейронной сети...")
     while Mistake_of_NET > 0.08:
@@ -237,15 +272,14 @@ def Learn():
 
 
 def start_checker() -> object:
+    Learn()
     while True:
         now = datetime.datetime.now()
-        if now.hour == 23 and now.minute == 9:
-            Learn()
+        # if now.hour == 20 - DeltaTime and now.minute == 46:
+        # Learn()
         if now.minute in [4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59] and now.second == 58 and (
                 955000 < now.microsecond <= 960000):
             change_curs()
 
 
-#start_checker()
-
-Learn()
+start_checker()
